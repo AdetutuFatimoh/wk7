a03da727589afde7167e63022cd5d671d6ec57b2
AdetutuFatimoh <adetutufatimoh@gmail.com>
2023-06-08 08:15:48 -0700

    feat:update

---
+++ flag.js
@@ -0,0 +1,37 @@
+let myName = 'Adetutu'
+
+myName = 'Ade'
+
+const school = 'univelcity'
+
+const state =`Ogun`
+
+const statement =`my name is  ${myName}
+
+        and i work at ${school} 
+        
+        and I am from ${state}`
+
+const statement2 = 'Mohammed Ali said \'suffer now, and live the rest  of he\'s your life a champion\''
+
+// alert(statement2)
+// confirm(statement2)
+
+// let age = prompt(' how old are you')
+
+// console.log(`You were born in ${new Date().getFullYear() - age}`)
+
+
+let name = prompt ('what is your name')
+
+console.log(`your name is Adetutu`)
+
+let age = prompt ('year of birth')
+
+let statement3 = (`Your name is ${name} and
+ you are ${new Date().getFullYear() - age} years
+ old`)
+
+ console.log(statement3)
+
+
